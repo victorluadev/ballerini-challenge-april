5bb40bc29eb87b8b8adc225702120534ceafed1d
victorluadev <streamyodin@gmail.com>
2023-04-07 22:49:09 -0300

    feat: customizando input

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,16 @@ function App() {
         <span>✍</span>
         <h1>Faça algo mágico...</h1>
         <h2>Ache sempre tudo em um só lugar!</h2>
+        <div className="searchContainer">
+          <span></span>
+          <input
+            type="text"
+            id="search"
+            name="search"
+            placeholder="Faça algo mágico..."
+            className="searchButton"
+          />
+        </div>
       </div>
     </React.Fragment>
   );
