f73c2d76f11b7460e61fadf585a718044639076e
victorluadev <streamyodin@gmail.com>
2023-04-10 19:59:15 -0300

    feat: fazendo a mágica acontecer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import { useState } from "react";
+import { IServers } from "./types/servers";
 
 function App() {
   const [filter, setFilter] = useState<string>("");
+  const [communities, setCommunities] = useState<IServers[]>([]);
+
+  let timer: NodeJS.Timeout;
+
+  function getServers(event: React.KeyboardEvent<HTMLInputElement>, filter: string) {
+    clearTimeout(timer);
+
+    console.log(event.key)
+    
+    if (filter.length >= 2 &&(event.key.match("^\\w$|\\s") || event.key === "Backspace")) {
+      timer = setTimeout(fetchData, 500);
+    }
+  }
+
+  async function fetchData() {
+    await fetch(
+      `https://discord.com/api//discovery/search?query=${filter}&limit=20`
+    )
+      .then((data) => data.json())
+      .then((data) => setCommunities(data.hits));
+  }
 
   return (
     <main>
@@ -17,6 +39,7 @@ function App() {
             name="search"
             value={filter}
             onChange={(e) => setFilter(e.target.value)}
+            onKeyUp={(e) => getServers(e, filter)}
             placeholder="Faça algo mágico..."
             className="searchButton"
           />
@@ -24,22 +47,28 @@ function App() {
         <div
           className={"searchResults " + (filter.length > 0 ? "show" : "hide")}
         >
-          <div className="itemContainer">
-            <img
-              src="https://www.grouphealth.ca/wp-content/uploads/2018/05/placeholder-image-300x225.png"
-              alt="nome do servidor"
-            />
-            <div className="itemDetails">
-              <h1>Comunidade top</h1>
-              <h2>20k membros</h2>
+          {communities.map((item, index) => (
+            <div className="itemContainer" key={index}>
+              <img
+                src={`https://cdn.discordapp.com/icons/${item.id}/${item.icon}.png`}
+                alt={`${item.name} Server`}
+              />
+              <div className="itemDetails">
+                <h1>{item.name}</h1>
+                <h2>{`${item.approximate_member_count} membros`}</h2>
+              </div>
+              <div className="itemActions">
+                <a
+                  href={`https://discord.gg/${item.vanity_url_code}`}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="linkInvite"
+                >
+                  <button className="buttonInvite">Entrar</button>
+                </a>
+              </div>
             </div>
-            <div className="itemActions">
-              <a href="#text" target="_blank" className="linkInvite" rel="noreferrer">
-              <button className="buttonInvite">Entrar
-              </button>
-              </a>
-            </div>
-          </div>
+          ))}
         </div>
       </section>
     </main>
---
+++ src/types/servers.ts
@@ -0,0 +1,8 @@
+export interface IServers {
+  id: string
+  name: string;
+  icon: string;
+  description: string;
+  vanity_url_code: string;
+  approximate_member_count: number;
+}
