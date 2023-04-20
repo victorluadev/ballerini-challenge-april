3a7801c3935249e1b6293cff2c2bedda23fed7e0
victorluadev <streamyodin@gmail.com>
2023-04-20 15:05:26 -0300

    feat: adicionando localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { IServers } from "./types/servers";
-import { ITheme } from "./types/themes";
+import { ITheme, TTheme } from "./types/themes";
 
 function App() {
-  const [theme, setTheme] = useState<ITheme>({actual: 'dark'});
+  const actualTheme = localStorage.getItem("theme")
+    ? { actual: localStorage.getItem("theme") as TTheme }
+    : { actual: "dark" as TTheme };
+
+  const [theme, setTheme] = useState<ITheme>(actualTheme);
   const [filter, setFilter] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
   const [communities, setCommunities] = useState<IServers[]>([]);
 
   let timer: NodeJS.Timeout;
 
-  function getServers(event: React.KeyboardEvent<HTMLInputElement>, filter: string) {
+  useEffect(() => {
+    localStorage.setItem("theme", theme.actual);
+  }, [theme]);
+
+  function getServers(
+    event: React.KeyboardEvent<HTMLInputElement>,
+    filter: string
+  ) {
     setLoading(true);
 
     console.log(loading);
     clearTimeout(timer);
 
-    console.log(event.key)
-    
+    console.log(event.key);
+
     if (filter.length >= 2 && event.key === "Enter") {
       timer = setTimeout(fetchData, 500);
 
-      
       console.log(loading);
     }
   }
@@ -36,21 +46,29 @@ function App() {
   }
 
   return (
-    <main className={`mainContainer ${theme.actual === 'dark' ? 'dark' : 'light'}`}>
+    <main
+      className={`mainContainer ${theme.actual === "dark" ? "dark" : "light"}`}
+    >
       <header className="headerContainer">
         <span className="headerItem">
-          {theme.actual === 'dark' ? (
-            <i onClick={() => setTheme({actual: 'light'})}> 🌞 </i>
-            ) : (
-            <i onClick={() => setTheme({actual: 'dark'})}> 🌙 </i>
+          {theme.actual === "dark" ? (
+            <i onClick={() => setTheme({ actual: "light" })}> 🌞 </i>
+          ) : (
+            <i onClick={() => setTheme({ actual: "dark" })}> 🌙 </i>
           )}
         </span>
       </header>
-      <section className={`container ${theme.actual === 'dark' ? 'dark' : 'light'}`} >
+      <section
+        className={`container ${theme.actual === "dark" ? "dark" : "light"}`}
+      >
         <span>✍</span>
         <h1>Faça algo mágico...</h1>
         <h2>Ache sempre tudo em um só lugar!</h2>
-        <div className={`searchContainer ${theme.actual === 'dark' ? 'dark' : 'light'}`}>
+        <div
+          className={`searchContainer ${
+            theme.actual === "dark" ? "dark" : "light"
+          }`}
+        >
           <span></span>
           <input
             type="text"
@@ -60,19 +78,34 @@ function App() {
             onChange={(e) => setFilter(e.target.value)}
             onKeyUp={(e) => getServers(e, filter)}
             placeholder="Faça algo mágico..."
-            className={`searchButton ${theme.actual === 'dark' ? 'dark' : 'light'}`}
+            className={`searchButton ${
+              theme.actual === "dark" ? "dark" : "light"
+            }`}
           />
         </div>
         <div
-          className={"searchResults " + (filter.length > 0 ? "show " : "hide ") + (theme.actual === 'dark' ? 'dark' : 'light')}
+          className={
+            "searchResults " +
+            (filter.length > 0 ? "show " : "hide ") +
+            (theme.actual === "dark" ? "dark" : "light")
+          }
         >
           {communities.map((item, index) => (
-            <div key={index} className={`itemContainer ${theme.actual === 'dark' ? 'dark' : 'light'}`}>
+            <div
+              key={index}
+              className={`itemContainer ${
+                theme.actual === "dark" ? "dark" : "light"
+              }`}
+            >
               <img
                 src={`https://cdn.discordapp.com/icons/${item.id}/${item.icon}.png`}
                 alt={`${item.name} Server`}
               />
-              <div className={`itemDetails ${theme.actual === 'dark' ? 'dark' : 'light'}`}>
+              <div
+                className={`itemDetails ${
+                  theme.actual === "dark" ? "dark" : "light"
+                }`}
+              >
                 <h1>{item.name}</h1>
                 <h2>{`${item.approximate_member_count} membros`}</h2>
               </div>
